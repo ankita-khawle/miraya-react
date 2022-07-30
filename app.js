8962f960701ebc825c909ebbd7d51752d8bd049a
ankita <khawleankita@gmail.com>
2022-07-31 00:13:41 +0530

    test

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
